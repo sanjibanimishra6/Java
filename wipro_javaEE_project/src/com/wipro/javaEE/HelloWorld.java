2ee19ed4e2970decf7c15aa7dfa56968a8b9478b
sanjibanimishra24 <112516498+sanjibanimishra24@users.noreply.github.com>
2025-02-01 10:18:57 +0530

    Commit

---
+++ wipro_javaEE_project/src/com/wipro/javaEE/HelloWorld.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.wipro.javaEE;
+
+/**
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("MY second program");
+	}
+
+}
