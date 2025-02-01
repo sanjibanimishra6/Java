f98506372cea19ad6c419aec765af5bd8265df97
sanjibanimishra24 <112516498+sanjibanimishra24@users.noreply.github.com>
2025-02-01 15:47:04 +0530

    Day1

---
+++ wipro_java_project/src/com/wipro/java/Flower.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.wipro.java;
+
+/**
+ * 
+ */
+public class Flower {
+
+	/**
+	 * 
+	 */
+	String color;
+	int petalnum ;
+	public Flower(String colour, int petalno) {
+		this.color = colour;
+		this.petalnum = petalno;
+	}
+void display() {
+	System.out.println("Flower Colour " + color + " No of petals " + petalnum);
+}
+	
+	public static void main(String[] args) {
+		Flower flowerobj1 = new Flower("BLue",5);
+		Flower flowerobj2 = new Flower("BLue",5);
+		
+		 flowerobj1 .display();
+		 flowerobj2.display(); 
+	}
+
+}
