0911f37cf158d1821ce0927b21520ff35a9c675a
sanjibanimishra24 <112516498+sanjibanimishra24@users.noreply.github.com>
2025-02-03 12:06:48 +0530

    day2

--- wipro_java_project/src/com/wipro/java/oops/Projectlead.java
+++ wipro_java_project/src/com/wipro/java/oops/Projectlead.java
@@ -18,4 +18,18 @@ public class Projectlead extends Employee{
 		// TODO Auto-generated constructor stub
 	}
 
-}
+
+public static void main(String[] args) {
+	//Child object is instantiated from child constructor
+	// Parent class consumes properties and behavior
+	
+	Employee emp = new  Projectlead();
+	emp.setEmp_no(1234);
+	emp.setEmail_ID("sanjibani@gmail.com");
+	emp.setEmp_name("Sanjibani");
+	emp.setSalary(50000f);
+	System.out.println(emp.getEmp_name());
+	System.out.println(emp.getEmail_ID());
+	System.out.println(emp.getEmp_no());
+	System.out.println(emp.getSalary());
+}}
