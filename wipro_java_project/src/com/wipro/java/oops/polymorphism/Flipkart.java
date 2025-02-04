963487ff7cb407de747f4e6f8988ce5260a1d8f3
sanjibanimishra24 <112516498+sanjibanimishra24@users.noreply.github.com>
2025-02-04 11:20:14 +0530

    Day3

---
+++ wipro_java_project/src/com/wipro/java/oops/polymorphism/Flipkart.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.wipro.java.oops.polymorphism;
+
+/**
+ * 
+ */
+public class Flipkart {
+
+	/**
+	 * 
+	 */
+	public Flipkart() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		   Phone phone = new MyMusic();
+	        phone.flipkart();
+	        phone.Wishlist();
+            phone.Ethnicwear();
+	}
+
+}
