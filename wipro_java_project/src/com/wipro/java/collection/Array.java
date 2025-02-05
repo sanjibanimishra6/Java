96eaa8adf5810289fe1d4c85454cea74e137f317
sanjibanimishra24 <112516498+sanjibanimishra24@users.noreply.github.com>
2025-02-05 16:20:04 +0530

    Day4

--- wipro_java_project/src/com/wipro/java/collection/Array.java
+++ wipro_java_project/src/com/wipro/java/collection/Array.java
@@ -9,10 +9,10 @@ public class Array {
 //allocating memory for 5 integers
    arr = new int[5];
   arr[0] = 10;
-  arr[0] = 20;
-  arr[0] = 30;
-  arr[0] = 40;
-  arr[0] = 50;
+  arr[1] = 20;
+  arr[2] = 30;
+  arr[3] = 40;
+  arr[4] = 50;
 
 //accessing elements from the specified array
 for(int i = 0; i < arr.length;i++) {
